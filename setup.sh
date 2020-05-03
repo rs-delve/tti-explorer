83fff9db8c0f4ab0696a123b0b935f5010a617fd
Michael Hutchinson <hutchinson.michael.john@gmail.com>
2020-05-03 15:57:34 +0100

    Pip can't install correct JAX versions

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 virtualenv -p python3.7 venv
 
-pip install -r requirment.txt   # CPU JAX Only. For GPUs will need to install seperatly.
+pip install -r requirments.txt   # CPU JAX Only. For GPUs will need to install seperatly.
