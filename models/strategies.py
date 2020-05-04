fcf49b1aed3db2521543c89d4cd4b015602521a0
bryn <4146799+brynhayder@users.noreply.github.com>
2020-05-04 18:19:06 +0100

    updating

--- models/strategies.py
+++ models/strategies.py
@@ -1,9 +1,8 @@
-
 from utils import Registry
 
 registry = Registry()
 
 # How to define a scenario
 @registry('no_measures')
-def function_that_does_no_measures(*args, **kwargs):
+def function_that_does_no_measures(case, contacts, *args, **kwargs):
     return 10
