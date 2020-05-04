952f3fb26cc2956272b424ba3cf74083e1135571
bryn <4146799+brynhayder@users.noreply.github.com>
2020-05-04 19:12:55 +0100

    made registry case insensitive

--- models/utils.py
+++ models/utils.py
@@ -1,14 +1,14 @@
 
-
 class Registry:
+    "Case insensitive registry"
     def __init__(self):
         self._register = dict()
     
     def __getitem__(self, key):
-        return self._register[key]
+        return self._register[key.lower()]
     
     def __call__(self, name):
         def add(thing):
-            self._register[name] = thing
+            self._register[name.lower()] = thing
             return thing
         return add
