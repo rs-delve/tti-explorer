f2abc20e69f6d4401ad95cf87654564374881322
Michael Hutchinson <hutchinson.michael.john@gmail.com>
2020-05-07 17:46:16 +0100

    Make data script

---
+++ make_data.h
@@ -0,0 +1,17 @@
+python models/generate_cases.py kucharski 50000 data/cases/kucharski_1.json --data-dir data --seed 1
+python models/generate_cases.py kucharski 50000 data/cases/kucharski_2.json --data-dir data --seed 2
+python models/generate_cases.py kucharski 50000 data/cases/kucharski_3.json --data-dir data --seed 3
+python models/generate_cases.py kucharski 50000 data/cases/kucharski_4.json --data-dir data --seed 4
+python models/generate_cases.py kucharski 50000 data/cases/kucharski_5.json --data-dir data --seed 5
+
+python models/generate_cases.py oxteam 50000 data/cases/oxteam_1.json --data-dir data --seed 1
+python models/generate_cases.py oxteam 50000 data/cases/oxteam_2.json --data-dir data --seed 2
+python models/generate_cases.py oxteam 50000 data/cases/oxteam_3.json --data-dir data --seed 3
+python models/generate_cases.py oxteam 50000 data/cases/oxteam_4.json --data-dir data --seed 4
+python models/generate_cases.py oxteam 50000 data/cases/oxteam_5.json --data-dir data --seed 5
+
+python models/generate_cases.py oxteam 230000 data/cases/oxteam_1_largge.json --data-dir data --seed 1
+python models/generate_cases.py oxteam 230000 data/cases/oxteam_2_largge.json --data-dir data --seed 2
+python models/generate_cases.py oxteam 230000 data/cases/oxteam_3_largge.json --data-dir data --seed 3
+python models/generate_cases.py oxteam 230000 data/cases/oxteam_4_largge.json --data-dir data --seed 4
+python models/generate_cases.py oxteam 230000 data/cases/oxteam_5_largge.json --data-dir data --seed 5
