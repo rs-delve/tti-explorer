6da5d91daec336f53431c459661a8c19f7c7efb8
Andrei Paleyes <apalees@gmail.com>
2020-05-13 09:41:48 +0100

    Use empty properly, add has_covid param

--- tests/test_strategies.py
+++ tests/test_strategies.py
@@ -8,12 +8,13 @@ from models import config
 
 TEST_RANDOM_SEED = 42
 
+@pytest.mark.parametrize("has_covid", [True, False])
 @pytest.mark.parametrize("s_level", list(config.S_levels.keys()))
 @pytest.mark.parametrize("contact_trace_option", list(config.contact_trace_options.keys()))
-def test_temporal_anne_flowchart_single_case_no_contacts(s_level, contact_trace_option):
+def test_temporal_anne_flowchart_single_case_no_contacts(has_covid, s_level, contact_trace_option):
     case = Case(
         under18=False,
-        covid=False,
+        covid=has_covid,
         symptomatic=False,
         has_app=False,
         report_nhs=False,
@@ -21,7 +22,7 @@ def test_temporal_anne_flowchart_single_case_no_contacts(s_level, contact_trace_
         day_noticed_symptoms=0,
         inf_profile=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
     )
-    contacts = Contacts(0, np.empty((1,2), dtype=int), np.empty((1,2), dtype=int), np.empty((1,2), dtype=int))
+    contacts = Contacts(0, np.empty((0,2), dtype=int), np.empty((0,2), dtype=int), np.empty((0,2), dtype=int))
     scenario = s_level + "_" + contact_trace_option
 
     try:
@@ -34,8 +35,12 @@ def test_temporal_anne_flowchart_single_case_no_contacts(s_level, contact_trace_
 
     result = temporal_anne_flowchart(case, contacts, rng, **parameters)
 
-    assert np.isnan(result[RETURN_KEYS.base_r])
-    assert np.isnan(result[RETURN_KEYS.reduced_r])
+    if has_covid:
+        result[RETURN_KEYS.base_r] == 0
+        result[RETURN_KEYS.reduced_r] == 0
+    else:
+        assert np.isnan(result[RETURN_KEYS.base_r])
+        assert np.isnan(result[RETURN_KEYS.reduced_r])
     assert result[RETURN_KEYS.man_trace] == 0
     assert result[RETURN_KEYS.app_trace] == 0
     assert result[RETURN_KEYS.tests] == 0
