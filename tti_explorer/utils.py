7939a1425ba05598e46bea0081c04279486975a1
Andrei Paleyes <apalees@gmail.com>
2020-05-26 11:38:00 +0100

    Remove experimental code

--- tti_explorer/utils.py
+++ tti_explorer/utils.py
@@ -12,9 +12,7 @@ ROOT_DIR = pathlib.Path(__file__).parent.parent
 
 
 def bool_bernoulli(p, rng):
-    #return bool(rng.binomial(1, p))
-    #return rng.uniform() < p
-    return rng.uniform() <= p
+    return bool(rng.binomial(1, p))
 
 
 def categorical(pvals, rng, n=1):
